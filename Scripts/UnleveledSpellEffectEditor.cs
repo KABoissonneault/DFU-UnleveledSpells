413f36d972a0184d1963d5f8f98279ab95d9ed5f
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2021-08-04 19:04:06 -0400

    Initial implementation

---
+++ Scripts/UnleveledSpellEffectEditor.cs
@@ -0,0 +1,236 @@
+using System;
+
+using UnityEngine;
+
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.MagicAndEffects;
+using DaggerfallWorkshop.Game.UserInterface;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
+using DaggerfallConnect.Arena2;
+using DaggerfallWorkshop.Game.Formulas;
+using DaggerfallWorkshop.Utility.AssetInjection;
+
+class UnleveledSpellEffectEditor : DaggerfallEffectSettingsEditorWindow
+{
+    protected TextLabel goldCostLabel;
+
+    #region Constructors
+
+    public UnleveledSpellEffectEditor(IUserInterfaceManager uiManager, DaggerfallBaseWindow previous = null)
+        : base(uiManager, previous)
+    {
+    }
+
+    #endregion
+
+    #region Setup Methods
+    protected override void Setup()
+    {
+        // Load all the textures used by effect editor window
+        LoadTextures();
+
+        // Setup native panel background
+        NativePanel.BackgroundTexture = baseTexture;
+
+        // Setup controls
+        SetupEffectDescriptionPanels();
+
+        exitButtonRect = new Rect(251, 147, 24, 16);
+
+        durationBaseSpinnerRect = new Rect(94, 106, spinnerWidth, spinnerHeight);
+        chanceBaseSpinnerRect = new Rect(94, 126, spinnerWidth, spinnerHeight);
+        magnitudeBaseMinSpinnerRect = new Rect(94, 146, spinnerWidth, spinnerHeight);
+        magnitudeBaseMaxSpinnerRect = new Rect(134, 146, spinnerWidth, spinnerHeight);
+
+        SetupSpinners();
+
+        SetupButtons();
+        InitControlState();
+
+        // Spell cost label
+        goldCostLabel = DaggerfallUI.AddTextLabel(DaggerfallUI.DefaultFont, new Vector2(244, 112), string.Empty, NativePanel);
+        spellCostLabel = DaggerfallUI.AddTextLabel(DaggerfallUI.DefaultFont, new Vector2(244, 132), string.Empty, NativePanel);
+
+        IsSetup = true;
+        UpdateCosts();
+    }
+
+    protected override void LoadTextures()
+    {
+        // Load source textures
+        TextureReplacement.TryImportImage("UnleveledSpellEffectEditorMask.png", true, out baseTexture);
+    }
+
+    protected override void SetupEffectDescriptionPanels()
+    {
+        // Create parent panel to house effect description
+        Panel descriptionParentPanel = DaggerfallUI.AddPanel(new Rect(5, 31, 312, 69), NativePanel);
+        descriptionParentPanel.HorizontalAlignment = HorizontalAlignment.Center;
+
+        // Create description panel centred inside of parent
+        descriptionPanel = DaggerfallUI.AddPanel(descriptionParentPanel, AutoSizeModes.None);
+        descriptionPanel.Size = new Vector2(306, 69);
+        descriptionPanel.HorizontalAlignment = HorizontalAlignment.Center;
+        descriptionPanel.VerticalAlignment = VerticalAlignment.Middle;
+        DaggerfallUI.Instance.SetDaggerfallPopupStyle(DaggerfallUI.PopupStyle.Parchment, descriptionPanel);
+
+        // Create multiline label for description text
+        descriptionLabel = new MultiFormatTextLabel();
+        descriptionLabel.HorizontalAlignment = HorizontalAlignment.Center;
+        descriptionLabel.VerticalAlignment = VerticalAlignment.Middle;
+        descriptionPanel.Components.Add(descriptionLabel);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    #endregion
+
+    #region Protected Methods
+
+    protected override void SetupSpinners()
+    {
+        // Add spinner controls
+        durationBaseSpinner = new UpDownSpinner(durationBaseSpinnerRect, spinnerUpButtonRect, spinnerDownButtonRect, spinnerValueLabelRect, 0, null, NativePanel);
+        chanceBaseSpinner = new UpDownSpinner(chanceBaseSpinnerRect, spinnerUpButtonRect, spinnerDownButtonRect, spinnerValueLabelRect, 0, null, NativePanel);
+        magnitudeBaseMinSpinner = new UpDownSpinner(magnitudeBaseMinSpinnerRect, spinnerUpButtonRect, spinnerDownButtonRect, spinnerValueLabelRect, 0, null, NativePanel);
+        magnitudeBaseMaxSpinner = new UpDownSpinner(magnitudeBaseMaxSpinnerRect, spinnerUpButtonRect, spinnerDownButtonRect, spinnerValueLabelRect, 0, null, NativePanel);
+        
+        // Set spinner mouse over colours
+        durationBaseSpinner.SetMouseOverBackgroundColor(hotButtonColor);
+        chanceBaseSpinner.SetMouseOverBackgroundColor(hotButtonColor);
+        magnitudeBaseMinSpinner.SetMouseOverBackgroundColor(hotButtonColor);
+        magnitudeBaseMaxSpinner.SetMouseOverBackgroundColor(hotButtonColor);
+        
+        // Set spinner ranges
+        durationBaseSpinner.SetRange(1, 60);
+        chanceBaseSpinner.SetRange(1, 100);
+        magnitudeBaseMinSpinner.SetRange(1, 999);
+        magnitudeBaseMaxSpinner.SetRange(1, 999);
+        
+        // Set spinner events
+        durationBaseSpinner.OnValueChanged += DurationBaseSpinner_OnValueChanged;
+        chanceBaseSpinner.OnValueChanged += ChanceBaseSpinner_OnValueChanged;
+        magnitudeBaseMinSpinner.OnValueChanged += MagnitudeBaseMinSpinner_OnValueChanged;
+        magnitudeBaseMaxSpinner.OnValueChanged += MagnitudeBaseMaxSpinner_OnValueChanged;
+    }
+
+    protected override void UpdateCosts()
+    {
+        RaiseSettingsChanged();
+
+        // Get spell cost
+        (int goldCost, int spellPointCost) = FormulaHelper.CalculateEffectCosts(EffectEntry);
+        goldCostLabel.Text = goldCost.ToString();
+        spellCostLabel.Text = spellPointCost.ToString();
+    }
+
+    protected override void InitControlState()
+    {
+        // Must have an effect template set
+        if (EffectTemplate == null)
+            throw new Exception(noEffectTemplateError);
+
+        // Get description text
+        TextFile.Token[] descriptionTokens = EffectTemplate.SpellMakerDescription;
+        if (descriptionTokens == null || descriptionTokens.Length == 0)
+            throw new Exception(string.Format("DaggerfallEffectSettingsEditorWindow: EffectTemplate {0} does not present any spellmaker description text.", EffectTemplate.Key));
+        else
+            descriptionLabel.SetText(descriptionTokens);
+
+        // Duration support
+        if (EffectTemplate.Properties.SupportDuration)
+        {
+            durationBaseSpinner.Enabled = true;
+        }
+        else
+        {
+            durationBaseSpinner.Enabled = false;
+        }
+
+        // Chance support
+        if (EffectTemplate.Properties.SupportChance)
+        {
+            chanceBaseSpinner.Enabled = true;
+        }
+        else
+        {
+            chanceBaseSpinner.Enabled = false;
+        }
+
+        // Magnitude support
+        if (EffectTemplate.Properties.SupportMagnitude)
+        {
+            magnitudeBaseMinSpinner.Enabled = true;
+            magnitudeBaseMaxSpinner.Enabled = true;
+        }
+        else
+        {
+            magnitudeBaseMinSpinner.Enabled = false;
+            magnitudeBaseMaxSpinner.Enabled = false;
+        }
+    }
+
+    protected override EffectEntry GetEffectEntry()
+    {
+        // Must have an effect template set
+        if (EffectTemplate == null)
+            throw new Exception(noEffectTemplateError);
+
+        // Create settings for effect
+        EffectSettings effectSettings = new EffectSettings();
+        if (IsSetup)
+        {
+            // Assign from UI control when setup
+            effectSettings.DurationBase = durationBaseSpinner.Value;
+            effectSettings.DurationPlus = 0;
+            effectSettings.DurationPerLevel = 1;
+            effectSettings.ChanceBase = chanceBaseSpinner.Value;
+            effectSettings.ChancePlus = 0;
+            effectSettings.ChancePerLevel = 1;
+            effectSettings.MagnitudeBaseMin = magnitudeBaseMinSpinner.Value;
+            effectSettings.MagnitudeBaseMax = magnitudeBaseMaxSpinner.Value;
+            effectSettings.MagnitudePlusMin = 0;
+            effectSettings.MagnitudePlusMax = 0;
+            effectSettings.MagnitudePerLevel = 1;
+        }
+
+        // Create entry
+        EffectEntry effectEntry = new EffectEntry();
+        effectEntry.Key = EffectTemplate.Key;
+        effectEntry.Settings = effectSettings;
+
+        return effectEntry;
+    }
+
+    protected override void SetEffectEntry(EffectEntry entry)
+    {
+        if (!IsSetup)
+            return;
+
+        // Assign effect template based on entry key
+        EffectTemplate = GameManager.Instance.EntityEffectBroker.GetEffectTemplate(entry.Key);
+        if (EffectTemplate == null)
+            throw new Exception(string.Format("SetEffectEntry() could not find effect key {0}", entry.Key));
+
+        // Assign settings to spinners
+        durationBaseSpinner.Value = entry.Settings.DurationBase;
+        chanceBaseSpinner.Value = entry.Settings.ChanceBase;
+        magnitudeBaseMinSpinner.Value = entry.Settings.MagnitudeBaseMin;
+        magnitudeBaseMaxSpinner.Value = entry.Settings.MagnitudeBaseMax;
+    }
+
+    protected override void SetSpinners(EffectSettings settings)
+    {
+        if (!IsSetup)
+            return;
+
+        durationBaseSpinner.Value = settings.DurationBase;
+        chanceBaseSpinner.Value = settings.ChanceBase;
+        magnitudeBaseMinSpinner.Value = settings.MagnitudeBaseMin;
+        magnitudeBaseMaxSpinner.Value = settings.MagnitudeBaseMax;
+    }
+
+    #endregion
+}
---
+++ Scripts/UnleveledSpells.cs
@@ -0,0 +1,275 @@
+using UnityEngine;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Utility.ModSupport;
+using System.Collections.Generic;
+using DaggerfallWorkshop.Game.MagicAndEffects;
+using DaggerfallWorkshop.Game.Formulas;
+using System;
+using DaggerfallWorkshop.Game.Entity;
+using System.Linq;
+using DaggerfallConnect;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
+
+namespace UnleveledSpellsMod
+{
+    public class UnleveledSpells : MonoBehaviour
+    {
+        private static Mod mod;
+
+        private readonly Dictionary<string, EffectCosts> durationCostOverride = new Dictionary<string, EffectCosts>();
+        private readonly Dictionary<string, EffectCosts> chanceCostOverride = new Dictionary<string, EffectCosts>();
+        private readonly Dictionary<string, EffectCosts> magnitudeCostOverride = new Dictionary<string, EffectCosts>();
+        private readonly Dictionary<string, float> factorOverride = new Dictionary<string, float>();
+
+        [Invoke(StateManager.StateTypes.Start, 0)]
+        public static void Init(InitParams initParams)
+        {
+            mod = initParams.Mod;
+
+            var go = new GameObject(mod.Title);
+            go.AddComponent<UnleveledSpells>();
+
+            mod.IsReady = true;
+        }
+
+        private void Start()
+        {
+            Debug.Log("Begin mod init: Unleveled Spells");
+
+            ParseCostOverrides();
+
+            FormulaHelper.RegisterOverride<Func<IEntityEffect, EffectSettings, DaggerfallEntity, FormulaHelper.SpellCost>>(mod, "CalculateEffectCosts", CalculateEffectCosts);
+
+            UIWindowFactory.RegisterCustomUIWindow(UIWindowType.EffectSettingsEditor, typeof(UnleveledSpellEffectEditor));
+
+            Debug.Log("Finished mod init: Unleveled Spells");
+        }
+
+        void ParseCostOverrides()
+        {
+            TextAsset costsFile = mod.GetAsset<TextAsset>("SpellCosts.csv");
+            if (costsFile == null)
+                return;
+
+            string[] lines = costsFile.text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines.Skip(1))
+            {
+                string[] tokens = line.Split(';');
+                string key = tokens[0];
+
+                // Duration override
+                if (tokens.Length >= 4 && !string.IsNullOrEmpty(tokens[1]))
+                {
+                    EffectCosts effectCosts = new EffectCosts();
+                    effectCosts.CostA = int.Parse(tokens[1]);
+
+                    if (int.TryParse(tokens[2], out int costB))
+                    {
+                        effectCosts.CostB = costB;
+                    }
+
+                    if (int.TryParse(tokens[3], out int offsetGold))
+                    {
+                        effectCosts.OffsetGold = offsetGold;
+                    }
+
+                    durationCostOverride.Add(key, effectCosts);
+                }
+
+                // Chance override
+                if (tokens.Length >= 7 && !string.IsNullOrEmpty(tokens[4]))
+                {
+                    EffectCosts effectCosts = new EffectCosts();
+                    effectCosts.CostA = int.Parse(tokens[4]);
+
+                    if (int.TryParse(tokens[5], out int costB))
+                    {
+                        effectCosts.CostB = costB;
+                    }
+
+                    if (int.TryParse(tokens[6], out int offsetGold))
+                    {
+                        effectCosts.OffsetGold = offsetGold;
+                    }
+
+                    chanceCostOverride.Add(key, effectCosts);
+                }
+
+                // Magnitude override
+                if (tokens.Length >= 10 && !string.IsNullOrEmpty(tokens[7]))
+                {
+                    EffectCosts effectCosts = new EffectCosts();
+                    effectCosts.CostA = int.Parse(tokens[7]);
+
+                    if (int.TryParse(tokens[8], out int costB))
+                    {
+                        effectCosts.CostB = costB;
+                    }
+
+                    if (int.TryParse(tokens[9], out int offsetGold))
+                    {
+                        effectCosts.OffsetGold = offsetGold;
+                    }
+
+                    magnitudeCostOverride.Add(key, effectCosts);
+                }
+
+                // Formula override
+                if (tokens.Length >= 11 && !string.IsNullOrEmpty(tokens[10]))
+                {
+                    factorOverride.Add(key, float.Parse(tokens[10]));
+                }
+            }
+        }
+
+        private static int GetEffectComponentCosts(
+            EffectCosts costs,
+            float starting,
+            float increase,
+            float perLevel)
+        {
+            //Calculate effect gold cost, spellpoint cost is calculated from gold cost after adding up for duration, chance and magnitude
+            return Mathf.RoundToInt(costs.OffsetGold + costs.CostA * starting + costs.CostB * increase / perLevel);
+        }
+
+        private FormulaHelper.SpellCost CalculateEffectCosts(IEntityEffect effect, EffectSettings settings, DaggerfallEntity casterEntity)
+        {
+            if(factorOverride.TryGetValue(effect.Key, out float factor))
+            {
+                float durationFactor = 1;
+                if(effect.Properties.SupportDuration)
+                {
+                    durationFactor = settings.DurationBase;
+                }
+
+                float chanceFactor = 1;
+                if(effect.Properties.SupportChance)
+                {
+                    chanceFactor = Mathf.Pow(settings.ChanceBase / 100.0f, 1.28f);
+                }
+
+                float magnitudeFactor = 1;
+                if(effect.Properties.SupportMagnitude)
+                {
+                    float magnitudeBase = (settings.MagnitudeBaseMin + settings.MagnitudeBaseMax) / 2.0f;
+                    magnitudeFactor = Mathf.Pow(magnitudeBase, 1.1f);
+                }
+
+                // Get related skill
+                int skillValue;
+                if (casterEntity == null)
+                {
+                    // From player
+                    skillValue = GameManager.Instance.PlayerEntity.Skills.GetLiveSkillValue((DFCareer.Skills)effect.Properties.MagicSkill);
+                }
+                else
+                {
+                    // From another entity
+                    skillValue = casterEntity.Skills.GetLiveSkillValue((DFCareer.Skills)effect.Properties.MagicSkill);
+                }
+
+                float factorResult = factor * durationFactor * chanceFactor * magnitudeFactor;
+
+                // Add gold costs together and calculate spellpoint cost from the result
+                FormulaHelper.SpellCost effectCost;
+                effectCost.goldCost = Mathf.RoundToInt(factorResult * 40.0f / 6.0f);
+                effectCost.spellPointCost = Mathf.RoundToInt(factorResult * (60 - (Math.Min(skillValue, 100) / 2)) / 60.0f);
+
+                return effectCost;
+            }
+            else
+            {
+                return ClassicEffectCosts(effect, settings, casterEntity);
+            }
+        }
+
+        private FormulaHelper.SpellCost ClassicEffectCosts(IEntityEffect effect, EffectSettings settings, DaggerfallEntity casterEntity)
+        {
+            bool activeComponents = false;
+
+            // Duration costs
+            int durationGoldCost = 0;
+            if (effect.Properties.SupportDuration)
+            {
+                EffectCosts durationCost = effect.Properties.DurationCosts;
+                if (durationCostOverride.TryGetValue(effect.Key, out EffectCosts costOverride))
+                {
+                    durationCost = costOverride;
+                }
+
+                activeComponents = true;
+                durationGoldCost = GetEffectComponentCosts(
+                    durationCost,
+                    settings.DurationBase,
+                    settings.DurationPlus,
+                    settings.DurationPerLevel);
+            }
+
+            // Chance costs
+            int chanceGoldCost = 0;
+            if (effect.Properties.SupportChance)
+            {
+                EffectCosts chanceCosts = effect.Properties.ChanceCosts;
+                if (chanceCostOverride.TryGetValue(effect.Key, out EffectCosts costOverride))
+                {
+                    chanceCosts = costOverride;
+                }
+
+                activeComponents = true;
+                chanceGoldCost = GetEffectComponentCosts(
+                    chanceCosts,
+                    settings.ChanceBase,
+                    settings.ChancePlus,
+                    settings.ChancePerLevel);
+            }
+
+            // Magnitude costs
+            int magnitudeGoldCost = 0;
+            if (effect.Properties.SupportMagnitude)
+            {
+                EffectCosts magnitudeCosts = effect.Properties.MagnitudeCosts;
+                if (magnitudeCostOverride.TryGetValue(effect.Key, out EffectCosts costOverride))
+                {
+                    magnitudeCosts = costOverride;
+                }
+
+                activeComponents = true;
+                float magnitudeBase = (settings.MagnitudeBaseMax + settings.MagnitudeBaseMin) / 2.0f;
+                float magnitudePlus = (settings.MagnitudePlusMax + settings.MagnitudePlusMin) / 2.0f;
+                magnitudeGoldCost = GetEffectComponentCosts(
+                    magnitudeCosts,
+                    magnitudeBase,
+                    magnitudePlus,
+                    settings.MagnitudePerLevel);
+            }
+
+            // If there are no active components (e.g. Teleport) then fudge some costs
+            // This gives the same casting cost outcome as classic and supplies a reasonable gold cost
+            // Note: Classic does not assign a gold cost when a zero-component effect is the only effect present, which seems like a bug
+            int fudgeGoldCost = 0;
+            if (!activeComponents)
+                fudgeGoldCost = 240;
+
+            // Get related skill
+            int skillValue;
+            if (casterEntity == null)
+            {
+                // From player
+                skillValue = GameManager.Instance.PlayerEntity.Skills.GetLiveSkillValue((DFCareer.Skills)effect.Properties.MagicSkill);
+            }
+            else
+            {
+                // From another entity
+                skillValue = casterEntity.Skills.GetLiveSkillValue((DFCareer.Skills)effect.Properties.MagicSkill);
+            }
+
+            // Add gold costs together and calculate spellpoint cost from the result
+            FormulaHelper.SpellCost effectCost;
+            effectCost.goldCost = durationGoldCost + chanceGoldCost + magnitudeGoldCost + fudgeGoldCost;
+            effectCost.spellPointCost = Mathf.RoundToInt(effectCost.goldCost * (60 - (Math.Min(skillValue, 100) / 2)) / 400.0f);
+
+            return effectCost;
+        }
+    }
+}
