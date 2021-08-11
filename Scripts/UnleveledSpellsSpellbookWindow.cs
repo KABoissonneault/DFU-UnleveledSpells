027e5033ec6f317bdfb3e6d51e866581957c496e
Kévin Alexandre Boissonneault <kaboissonneault@gmail.com>
2021-08-11 12:44:54 -0400

    Changed spell costs in the spell vendor to reflect the spellmaker cost, as opposed to just "spell point cost times 4". Spell vendor is still cheaper than spellmaker for the same spell, since you can haggle.

--- Scripts/UnleveledSpellsSpellbookWindow.cs
+++ Scripts/UnleveledSpellsSpellbookWindow.cs
@@ -1,3 +1,7 @@
+using DaggerfallWorkshop;
+using DaggerfallWorkshop.Game;
+using DaggerfallWorkshop.Game.Formulas;
+using DaggerfallWorkshop.Game.MagicAndEffects;
 using DaggerfallWorkshop.Game.UserInterface;
 using DaggerfallWorkshop.Game.UserInterfaceWindows;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
@@ -25,7 +29,7 @@ namespace UnleveledSpellsMod
             base.Update();
 
             // Handle hotkey assignment
-            if(spellsListBox.SelectedIndex != -1 && hasHotkeyBar)
+            if(!buyMode && spellsListBox.SelectedIndex != -1 && hasHotkeyBar)
             {
                 int maxHotkeySize = 0;
                 ModManager.Instance.SendModMessage("Hotkey Bar", "GetMaxHotkeyBarSize", null, (string _, object result) => { maxHotkeySize = (int)result; });
@@ -49,6 +53,77 @@ namespace UnleveledSpellsMod
                     }
                 }
             }
+        }
+
+        protected override void UpdateSelection()
+        {
+            // Update spell list scroller
+            spellsListScrollBar.Reset(spellsListBox.RowsDisplayed, spellsListBox.Count, spellsListBox.ScrollIndex);
+            spellsListScrollBar.TotalUnits = spellsListBox.Count;
+            spellsListScrollBar.ScrollIndex = spellsListBox.ScrollIndex;
+
+            // Get spell settings selected from player spellbook or offered spells
+            EffectBundleSettings spellSettings;
+            if (buyMode)
+            {
+                spellSettings = offeredSpells[spellsListBox.SelectedIndex];
+
+                // Kab: change gold cost to actually reflect the formula         
+                (int goldCost, int _) = FormulaHelper.CalculateTotalEffectCosts(spellSettings.Effects, spellSettings.TargetType);
+                presentedCost = goldCost;
+
+                // Presented cost is halved on Witches Festival holiday
+                uint gameMinutes = DaggerfallUnity.Instance.WorldTime.DaggerfallDateTime.ToClassicDaggerfallTime();
+                int holidayID = FormulaHelper.GetHolidayId(gameMinutes, 0);
+                if (holidayID == (int)DaggerfallConnect.DFLocation.Holidays.Witches_Festival)
+                {
+                    presentedCost >>= 1;
+                    if (presentedCost == 0)
+                        presentedCost = 1;
+                }
+
+                spellCostLabel.Text = presentedCost.ToString();
+            }
+            else
+            {
+                // Get spell and exit if spell index not found
+                if (!GameManager.Instance.PlayerEntity.GetSpell(spellsListBox.SelectedIndex, out spellSettings))
+                {
+                    spellNameLabel.Text = string.Empty;
+                    ClearEffectLabels();
+                    ShowIcons(false);
+                    return;
+                }
+            }
+
+            // Update spell name label
+            spellNameLabel.Text = spellSettings.Name;
+
+            // Update effect labels
+            if (spellSettings.Effects != null && spellSettings.Effects.Length > 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i < spellSettings.Effects.Length)
+                        SetEffectLabels(spellSettings.Effects[i].Key, i);
+                    else
+                        SetEffectLabels(string.Empty, i);
+                }
+            }
+            else
+            {
+                SetEffectLabels(string.Empty, 0);
+                SetEffectLabels(string.Empty, 1);
+                SetEffectLabels(string.Empty, 2);
+            }
+
+            // Update spell icons
+            spellIconPanel.BackgroundTexture = GetSpellIcon(spellSettings.Icon);
+            spellTargetIconPanel.BackgroundTexture = GetSpellTargetIcon(spellSettings.TargetType);
+            spellTargetIconPanel.ToolTipText = GetTargetTypeDescription(spellSettings.TargetType);
+            spellElementIconPanel.BackgroundTexture = GetSpellElementIcon(spellSettings.ElementType);
+            spellElementIconPanel.ToolTipText = GetElementDescription(spellSettings.ElementType);
+            ShowIcons(true);
         }
     }
 }
