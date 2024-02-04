a1a54d2ff6f4306365d0a72d7d46899d9a38f066
kaboissonneault <kaboissonneault@gmail.com>
2024-02-04 10:05:56 -0500

    Added "UESP page" generation command to us_dump

--- Scripts/UnleveledSpellsCommands.cs
+++ Scripts/UnleveledSpellsCommands.cs
@@ -13,8 +13,7 @@ using DaggerfallConnect.Save;
 using System.Linq;
 using DaggerfallWorkshop.Game.Entity;
 using DaggerfallWorkshop.Utility;
-using static UnityEngine.AudioSettings;
-using UnityEngine;
+using System.Text;
 
 namespace UnleveledSpellsMod
 {
@@ -25,6 +24,21 @@ namespace UnleveledSpellsMod
 #if UNITY_EDITOR
             ConsoleCommandsDatabase.RegisterCommand("us_dump", UnleveledSpellsCommands.Dump, "Dumps Unleveled Spells info to files");
 #endif
+        }
+
+        static string AddSpacesToSentence(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return "";
+            StringBuilder newText = new StringBuilder(text.Length * 2);
+            newText.Append(text[0]);
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (char.IsUpper(text[i]) && text[i - 1] != ' ')
+                    newText.Append(' ');
+                newText.Append(text[i]);
+            }
+            return newText.ToString();
         }
 
 #if UNITY_EDITOR
@@ -50,14 +64,14 @@ namespace UnleveledSpellsMod
                 {
                     outputFile.WriteLine("Index;Name;Element;Range;E1;E1 Dur;E1 %;E1 Mag Min;E1 Mag Max;E2;E2 Dur;E2 %;E2 Mag Min;E2 Mag Max;E3;E3 Dur;E3 %; E3 Mag Min; E3 Mag Max;Cost 5;Cost 50; Cost 100");
 
-                    foreach(fsData record in listRecords)
+                    foreach (fsData record in listRecords)
                     {
                         var fields = record.AsDictionary;
                         ElementTypes elementType = (ElementTypes)(1 << (int)fields["element"].AsInt64);
                         TargetTypes targetType = (TargetTypes)(1 << (int)fields["rangeType"].AsInt64);
-                        
+
                         string spellHeader = string.Join(";", fields["index"], fields["spellName"], elementType.ToString(), targetType.ToString());
-                        
+
                         var effects = fields["effects"].AsList;
 
                         string ToEffectStr(Dictionary<string, fsData> effectData)
@@ -84,7 +98,7 @@ namespace UnleveledSpellsMod
                         int cost5Total = 0;
                         int cost50Total = 0;
                         int cost100Total = 0;
-                        foreach(var effect in effects)
+                        foreach (var effect in effects)
                         {
                             var effectData = effect.AsDictionary;
                             var effectType = effectData["type"].AsInt64;
@@ -104,7 +118,7 @@ namespace UnleveledSpellsMod
 
                             EffectEntry effectEntry = new EffectEntry();
                             effectEntry.Key = effectTemplate.Key;
-                            if(effectTemplate.Properties.SupportDuration)
+                            if (effectTemplate.Properties.SupportDuration)
                             {
                                 effectEntry.Settings.DurationBase = (int)effectData["durationBase"].AsInt64;
                                 effectEntry.Settings.DurationPerLevel = 1;
@@ -140,6 +154,165 @@ namespace UnleveledSpellsMod
 
                         string line = string.Join(";", spellHeader, effect1Str, effect2Str, effect3Str, cost5Total.ToString(), cost50Total.ToString(), cost100Total.ToString());
                         outputFile.WriteLine(line);
+                    }
+                }
+
+                outputPath = Path.Combine(DaggerfallUnity.Settings.PersistentDataPath, "us_spell_uesp.txt");
+                using (StreamWriter outputFile = new StreamWriter(outputPath))
+                {
+                    var effectBroker = GameManager.Instance.EntityEffectBroker;
+
+                    IEnumerable<SpellRecord.SpellRecordData> standardSpells = effectBroker.StandardSpells;
+                    var spellsDB = standardSpells.Where(spell => spell.spellName != "Holy Touch").ToDictionary(spell => spell.index);
+
+                    void WriteSpell(int spellId, bool anchor)
+                    {
+                        var spell = spellsDB[spellId];
+                        
+                        outputFile.WriteLine("|-");
+
+                        var spellName = spell.spellName.Replace("!", "");
+
+                        if (anchor)
+                        {
+                            outputFile.Write("|{{Anchor|");
+                            outputFile.Write(spell.spellName);
+                            outputFile.Write("}}||");
+                        }
+                        else
+                        {
+                            outputFile.Write($"|{spell.spellName}||");
+                        }
+
+                        outputFile.Write(effectBroker.ClassicElementIndexToElementType(spell.element).ToString());
+                        outputFile.Write("||");
+
+                        outputFile.Write(AddSpacesToSentence(effectBroker.ClassicTargetIndexToTargetType(spell.rangeType).ToString()));
+                        outputFile.Write("||");
+
+                        var effects = spell.effects;
+                        var effectTemplates = effects.Select(effect =>
+                        {
+                            var effectType = effect.type;
+                            var effectSubtype = effect.subType;
+                            effectSubtype = (effectSubtype < 0) ? 255 : effectSubtype; // Entity effect keys use 255 instead of -1 for subtype
+
+                            int classicKey = BaseEntityEffect.MakeClassicKey((byte)effectType, (byte)effectSubtype);
+                            return GameManager.Instance.EntityEffectBroker.GetEffectTemplate(classicKey);
+                        }).ToArray();
+
+                        int effectCount = effects.Length;
+                        string durations = string.Join("<br>", effects.Select((effect, index) =>
+                        {
+                            var template = effectTemplates[index];
+                            if (template.Properties.SupportDuration)
+                            {
+                                return effect.durationBase.ToString();
+                            }
+                            else
+                            {
+                                return "-";
+                            }
+                        }));
+
+                        string chances = string.Join("<br>", effects.Select((effect, index) =>
+                        {
+                            var template = effectTemplates[index];
+                            if (template.Properties.SupportChance)
+                            {
+                                return effect.chanceBase.ToString();
+                            }
+                            else
+                            {
+                                return "-";
+                            }
+                        }));
+
+                        string magnitudes = string.Join("<br>", effects.Select((effect, index) =>
+                        {
+                            var template = effectTemplates[index];
+                            if (template.Properties.SupportMagnitude)
+                            {
+                                return effect.magnitudeBaseLow.ToString() + "-" + effect.magnitudeBaseHigh.ToString();
+                            }
+                            else
+                            {
+                                return "-";
+                            }
+                        }));
+
+                        string names = string.Join("<br>", effects.Select((effect, index) =>
+                        {
+                            var template = effectTemplates[index];
+                            return template.DisplayName;
+                        }));
+
+                        outputFile.Write(string.Join("||", durations, chances, magnitudes, names));
+                        outputFile.WriteLine();
+                    }
+
+                    // Write standard spells
+                    foreach (fsData record in listRecords)
+                    {
+                        var fields = record.AsDictionary;
+                        string spellName = fields["spellName"].AsString;
+                        if (spellName.StartsWith("!"))
+                            continue;
+
+                        int index = (int)fields["index"].AsInt64;
+                        WriteSpell(index, anchor: true);
+                    }
+                    
+                    // Write enemies spells
+                    
+                    Mod daggerfallEnemyExpansion = ModManager.Instance.GetModFromGUID("76557441-7025-402e-a145-e3e1a28a093d"); // Daggerfall Enemy Expansion
+
+                    foreach(var enemy in EnemyBasics.Enemies)
+                    {
+                        if (DaggerfallEntity.IsClassEnemyId(enemy.ID))
+                            continue;
+
+                        var spells = UnleveledEnemySpells.GetEnemySpells(enemy.ID, enemy.Level);
+                        if (spells != null)
+                        {
+                            outputFile.WriteLine();
+                            string enemyName = TextManager.Instance.GetLocalizedEnemyName(enemy.ID);
+                            outputFile.WriteLine($"{enemyName}:");
+                            foreach (byte spell in spells)
+                            {                                
+                                WriteSpell(spell, anchor: false);
+                            }
+                        }
+                        else if(daggerfallEnemyExpansion != null)
+                        {
+                            ModManager.Instance.SendModMessage(daggerfallEnemyExpansion.Title, "getEnemySpells", Tuple.Create(enemy.ID, enemy.Level), (_, result) =>
+                            {
+                                var customSpells = (int[])result;
+                                if(customSpells != null)
+                                {
+                                    outputFile.WriteLine();
+                                    string enemyName = TextManager.Instance.GetLocalizedEnemyName(enemy.ID);
+                                    outputFile.WriteLine($"{enemyName}:");
+                                    foreach (int spell in customSpells)
+                                    {
+                                        WriteSpell(spell, anchor: false);
+                                    }
+                                }
+                            });
+                        }
+                    }
+
+                    // Class enemies
+                    for(int i = 0; i < 7; ++i)
+                    {
+                        int level = Math.Max(i * 3, 1);
+                        var spells = UnleveledEnemySpells.GetEnemySpells((int)MobileTypes.Mage, level);
+                        outputFile.WriteLine();
+                        outputFile.WriteLine($"Class {level}:");
+                        foreach (byte spell in spells)
+                        {
+                            WriteSpell(spell, anchor: false);
+                        }
                     }
                 }
             }
